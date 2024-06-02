c1b30a48e9eabeb5884ae16a237424e68b1880a0
Heiner000 <garrett.heiner@gmail.com>
2024-06-01 17:53:39 -0700

    adds corner snapping

--- MainForm.cs
+++ MainForm.cs
@@ -2,21 +2,55 @@ using System;
 using System.Windows.Forms;
 using NHotkey;
 using NHotkey.WindowsForms;
+using System.Drawing;
 
 namespace WindowManager
 {
     public partial class MainForm : Form
     {
+        private NotifyIcon trayIcon = new NotifyIcon();
+        private ContextMenuStrip trayMenu = new ContextMenuStrip();
+
         public MainForm()
         {
             InitializeComponent();
             RegisterHotkeys();
+            InitializeTrayIcon();
         }
 
         private void RegisterHotkeys()
         {
             HotkeyManager.Current.AddOrReplace("SnapLeft", Keys.Left | Keys.Control | Keys.Alt, OnSnapLeft);
             HotkeyManager.Current.AddOrReplace("SnapRight", Keys.Right | Keys.Control | Keys.Alt, OnSnapRight);
+            HotkeyManager.Current.AddOrReplace("SnapTop", Keys.Up | Keys.Control | Keys.Alt, OnSnapTop);
+            HotkeyManager.Current.AddOrReplace("SnapBottom", Keys.Down | Keys.Control | Keys.Alt, OnSnapBottom);
+            HotkeyManager.Current.AddOrReplace("SnapTopLeft", Keys.O | Keys.Control | Keys.Alt, OnSnapTopLeft);
+            HotkeyManager.Current.AddOrReplace("SnapTopRight", Keys.P | Keys.Control | Keys.Alt, OnSnapTopRight);
+            HotkeyManager.Current.AddOrReplace("SnapBottomLeft", Keys.K | Keys.Control | Keys.Alt, OnSnapBottomLeft);
+            HotkeyManager.Current.AddOrReplace("SnapBottomRight", Keys.L | Keys.Control | Keys.Alt, OnSnapBottomRight);
+        }
+
+        private void InitializeTrayIcon()
+        {
+            trayMenu.Items.Add("Exit", null, OnExit);
+            
+            trayIcon.Text = "WindowManager";
+            trayIcon.Icon = new Icon(SystemIcons.Application, 40, 40);
+
+            trayIcon.ContextMenuStrip = trayMenu;
+            trayIcon.Visible = true;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Visible = false; // hide the form window
+            ShowInTaskbar = false; // remove from taskbar
+            base.OnLoad(e);
+        }
+
+        private void OnExit(object? sender, EventArgs e)
+        {
+            Application.Exit();
         }
 
         private void OnSnapLeft(object? sender, HotkeyEventArgs e)
@@ -30,6 +64,48 @@ namespace WindowManager
         {
             var screenSize = WindowManager.GetScreenSize();
             WindowManager.MoveAndResizeWindow(screenSize.width / 2, 0, screenSize.width / 2, screenSize.height);
+            e.Handled = true;
+        }
+
+        private void OnSnapTop(object? sender, HotkeyEventArgs e)
+        {
+            var screenSize = WindowManager.GetScreenSize();
+            WindowManager.MoveAndResizeWindow(0, 0, screenSize.width, screenSize.height / 2);
+            e.Handled = true;
+        }
+
+        private void OnSnapBottom(object? sender, HotkeyEventArgs e)
+        {
+            var screenSize = WindowManager.GetScreenSize();
+            WindowManager.MoveAndResizeWindow(0, screenSize.height / 2, screenSize.width, screenSize.height / 2);
+            e.Handled = true;
+        }
+
+        private void OnSnapTopLeft(object? sender, HotkeyEventArgs e)
+        {
+            var screenSize = WindowManager.GetScreenSize();
+            WindowManager.MoveAndResizeWindow(0, 0, screenSize.width / 2, screenSize.height / 2);
+            e.Handled = true;
+        }
+
+        private void OnSnapTopRight(object? sender, HotkeyEventArgs e)
+        {
+            var screenSize = WindowManager.GetScreenSize();
+            WindowManager.MoveAndResizeWindow(screenSize.width / 2, 0, screenSize.width / 2, screenSize.height / 2);
+            e.Handled = true;
+        }
+
+        private void OnSnapBottomLeft(object? sender, HotkeyEventArgs e)
+        {
+            var screenSize = WindowManager.GetScreenSize();
+            WindowManager.MoveAndResizeWindow(0, screenSize.height / 2, screenSize.width / 2, screenSize.height / 2);
+            e.Handled = true;
+        }
+
+        private void OnSnapBottomRight(object? sender, HotkeyEventArgs e)
+        {
+            var screenSize = WindowManager.GetScreenSize();
+            WindowManager.MoveAndResizeWindow(screenSize.width / 2, screenSize.height / 2, screenSize.width / 2, screenSize.height / 2);
             e.Handled = true;
         }
     }
--- WindowManager.cs
+++ WindowManager.cs
@@ -30,19 +30,31 @@ namespace WindowManager
         public static void MoveAndResizeWindow(int x, int y, int width, int height)
         {
             IntPtr hWnd = NativeMethods.GetForegroundWindow();
-            NativeMethods.MoveWindow(hWnd, x, y, width, height, true);
+            if (hWnd != IntPtr.Zero)
+            {
+                NativeMethods.MoveWindow(hWnd, x, y, width, height, true);
+            }
         }
 
         public static (int width, int height) GetScreenSize()
         {
-            return (Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            var primaryScreen = Screen.PrimaryScreen;
+            if (primaryScreen != null)
+            {
+                return (primaryScreen.Bounds.Width, primaryScreen.Bounds.Height);
+            }
+            return (0, 0);
         }
 
         public static (int x, int y, int width, int height) GetWindowRect()
         {
             IntPtr hWnd = NativeMethods.GetForegroundWindow();
-            NativeMethods.GetWindowRect(hWnd, out NativeMethods.RECT rect);
-            return (rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top);
+            if (hWnd != IntPtr.Zero)
+            {
+                NativeMethods.GetWindowRect(hWnd, out NativeMethods.RECT rect);
+                return (rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top);
+            }
+            return (0, 0, 0, 0);
         }
     }
 }
